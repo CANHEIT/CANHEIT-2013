4cb92d3511c3e4b58dcfa0fd2ea7600755297496
Pascal Laliberté <plaliberte@uOttawa.ca>
2012-10-25 17:09:58 -0400

    - Hooked up the subscribe form with the backend - Added the ajax-loader.gif - Adjusted the styling of the subscribe form, its error messages and success message - Added styling for input[type=submit]

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 
+jQuery(document).ready(function(){
+
+	$('#subscribeform').append('<div class="subscribe-loader hidden" />');
+
+	$('#subscribeform').submit(function(){
+
+		var action = $(this).attr('action');
+
+		$("#mesaj").slideUp(750,function() {
+		$('#mesaj').hide();
+
+ 		$('#subsubmit')
+			.attr('disabled','disabled');
+
+		$('#subscribeform div.subscribe-loader').fadeIn('slow');
+		
+
+		$.post(action, {
+			email: $('#subemail').val()
+		},
+			function(data){
+				document.getElementById('mesaj').innerHTML = data;
+				$('#mesaj').slideDown('slow');
+				$('#subscribeform div.subscribe-loader').fadeOut('slow');
+				$('#subsubmit').removeAttr('disabled');
+				if(data.match('success') != null) $('#subscribeform').slideUp('slow');
+
+			}
+		);
+
+		});
+
+		return false;
+
+	});
+
+});
