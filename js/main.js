d62b15f06501a073536dc37cab06dc0fff6a2056
Pascal Laliberté <plaliberte@uOttawa.ca>
2012-10-26 13:47:45 -0400

    - Modified all click tracking to use event tracking instead - Replaced hard-coded onclick attributes with rel="external" or rel="email" with a data-ga-label attribute for the action parameter. - Added jquery code to parse all a[rel=external] and a[rel=email] links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+jQuery(document).ready(function(){
+
+  function recordOutboundLink(link, category, action) {
+    try {
+      var myTracker=_gat._getTrackerByName();
+        _gaq.push(['myTracker._trackEvent', category , action ]);
+        setTimeout('document.location = "' + link.href + '"', 100)
+    } catch(err) {}
+  }
+  
+  $('a[rel=external]').each(function(){
+    var category = 'outbound-links';
+    var action = '';
+    if ($(this).data('ga-label')) {
+      $(this).click(function() {
+        action = $(this).data('ga-label')
+        recordOutboundLink(this,category,action);
+        return false;
+      })
+    } else if ($(this).attr('href')) {
+      $(this).click(function() {
+        action = $(this).attr('href')
+        recordOutboundLink(this,category,action);
+        return false;
+      })
+    }
+  });
+
+  $('a[rel=email]').each(function(){
+    var category = 'email';
+    var action = '';
+    if ($(this).data('ga-label')) {
+      $(this).click(function() {
+        action = $(this).data('ga-label')
+        recordOutboundLink(this,category,action);
+        return false;
+      })
+    } else if ($(this).attr('href')) {
+      $(this).click(function() {
+        action = $(this).attr('href')
+        recordOutboundLink(this,category,action);
+        return false;
+      })
+    }
+  });
+  
+});
+
 
 jQuery(document).ready(function(){
 
