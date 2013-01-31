f3ea38d090673b4558874bf0040e18cafc8144b7
Pascal Laliberté <plaliberte@uOttawa.ca>
2013-01-31 16:48:25 -0500

    - Renamed .json-cache/ to json-cache/ - Modified the json2html.php to take parameters: src of the data and template - Added an .htaccess to accommodations/ to rewrite the requests to json2html.php - Removed the static copies of the accommodations entries

---
+++ json-cache/update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o 550777.json http://gears.guidebook.com/api/v1/poi/550777/?category=14833&format=json&username=jarsenea@uottaw%20a.ca&api_key=DYJmr8vDWBrfZeBUr8wfgrhxMQUemRvnvSGYnfdKDQQxsvY 
--- json2html.php
+++ json2html.php
@@ -5,27 +5,17 @@ ini_set('display_errors', 1);
 
 require_once 'lib/vendor/autoload.php';
 
-//$json_url = "http://plaliberte@uOttawa.ca:".urlencode("JYsdp89yV?CpiH=LVRer")."@gears.guidebook.com/api/v1/poi/571968/?category=14833&format=json&username=jarsenea@uottawa.ca&api_key=DYJmr8vDWBrfZeBUr8wfgrhxMQUemRvnvSGYnfdKDQQxsvY";
+$cache_dir = 'json-cache/';
 
-$json_url = ".json-cache/532628.json";
+$json_url = isset($_GET['src'])? $cache_dir . $_GET['src'] . '.json' : "http://gears.guidebook.com/api/v1/poi/571968/?category=14833&format=json&username=jarsenea@uottawa.ca&api_key=DYJmr8vDWBrfZeBUr8wfgrhxMQUemRvnvSGYnfdKDQQxsvY";
 
-//echo $json_url;
+$template = isset($_GET['template'])? $_GET['template'] : 'accommodation.twig';
 
-//$options = array(
-//  'http'=>array(
-//    'method'=>"GET",
-//    'header'=>"Accept-language: en\r\n" .
-//              "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" .
-//              "User-Agent: Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25\r\n"
-//  )
-//);
-//
-//$context = stream_context_create($options);
+//$json_url = ".json-cache/532628.json";
 
 $loader = new Twig_Loader_Filesystem('templates/');
 $twig = new Twig_Environment($loader);
 
-//$json = file_get_contents($json_url, false, $context);
 $json = file_get_contents($json_url);
 //var_dump($json);
 $json = utf8_encode($json); 
@@ -34,6 +24,6 @@ $data = json_decode($json, true);
 
 //echo "<pre>".print_r($data,true)."</pre>";
 
-echo $twig->render('accommodation.twig', $data);
+echo $twig->render($template, $data);
 
 ?>
