490f207f4a88518b2c3adfbc347521de417d3c57
Pascal Laliberté <plaliberte@uOttawa.ca>
2013-02-01 10:49:50 -0500

    - Moved json2html.php to php/render-page.php

--- php/render-page.php
+++ php/render-page.php
@@ -3,9 +3,9 @@
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 
-require_once 'lib/vendor/autoload.php';
+require_once '../lib/vendor/autoload.php';
 
-$cache_dir = 'json-cache/';
+$cache_dir = '../json-cache/';
 
 $json_url = isset($_GET['src'])? $cache_dir . $_GET['src'] . '.json' : "http://gears.guidebook.com/api/v1/poi/571968/?category=14833&format=json&username=jarsenea@uottawa.ca&api_key=DYJmr8vDWBrfZeBUr8wfgrhxMQUemRvnvSGYnfdKDQQxsvY";
 
@@ -13,7 +13,7 @@ $template = isset($_GET['template'])? $_GET['template'] : 'accommodation.twig';
 
 //$json_url = ".json-cache/532628.json";
 
-$loader = new Twig_Loader_Filesystem('templates/');
+$loader = new Twig_Loader_Filesystem('../templates/');
 $twig = new Twig_Environment($loader);
 
 $json = file_get_contents($json_url);
