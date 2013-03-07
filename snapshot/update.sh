8275f1b62cb483adb2cad993efe03bcbced54a54
Pascal Laliberté <plaliberte@uOttawa.ca>
2013-03-07 17:34:41 -0500

    In the update.sh script, fixed the broken paths to the *.png, lib, 404.html, etc.

--- snapshot/update.sh
+++ snapshot/update.sh
@@ -2,4 +2,4 @@
 
 rm -Rf localhost\:8080/* localhost\:8080/.htaccess
 wget -mpc --no-cache --user-agent="" -e robots=off http://localhost:8080/ > wget.log 2>&1
-cp -Rfp *.png 404.html .htaccess php lib render-page.php localhost\:8080/
+cp -Rfp ../*.png ../404.html ../.htaccess ../php ../lib ../render-page.php localhost\:8080/
