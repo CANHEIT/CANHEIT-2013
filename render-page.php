64856a0dac4abb8307e706e9dcdaae5fbf304854
Nancy Hebert <nhebert@uottawa.ca>
2013-04-02 09:26:25 -0400

    Revert "- Added the geeven (mascot) photo on the about page"
    This reverts commit 4b92d6da12c1deed63ab410b291607ada36aa3ab.

--- render-page.php
+++ render-page.php
@@ -70,7 +70,7 @@
         , $p, $matches) ? true : false
       ) :
       $json_uri = '/api/v1/poi/' . $matches[2] . '/?category=13617&';
-      $template_file = $matches[1].'/local-eat.twig';
+      $template_file = $matches[1].'/restaurant.twig';
       array_push($parse_functions, 'fetch_links');
       break;
     case (
