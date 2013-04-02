5c19e1e9a41b888af0558a553f17aa23131beefc
Pascal Laliberté <plaliberte@uOttawa.ca>
2013-04-02 15:07:29 -0400

    Renamed the app promo banner to "app-promo" instead of "crowdvote" - Also made sure the app promo banner only displays on the desktop

--- js/downloadMobile.js
+++ js/downloadMobile.js
@@ -3,7 +3,7 @@
    $(function() {
             
             if($.cookie('crowdvoting') == 'never' || $.cookie('crowdvoting') == 'not-today') {
-              $('#crowdvote').hide(0);
+              $('#app-promo').hide(0);
               return;
             }
             
@@ -11,12 +11,12 @@
             
             $('#not-today').click(function(){
               $.cookie('crowdvoting','not-today',{ expires: 1 });
-              $('#crowdvote').fadeOut('slow');
+              $('#app-promo').fadeOut('slow');
             });
             
             $('#never').click(function(){
               $.cookie('crowdvoting','never',{ expires: 365 });
-              $('#crowdvote').fadeOut('slow');
+              $('#app-promo').fadeOut('slow');
             });
             
           });
