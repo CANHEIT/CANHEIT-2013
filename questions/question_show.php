877b80e948e8171d8fc60dceecd62ef3b78b8764
nhebert <nhebert@uottawa.ca>
2013-05-29 09:55:14 -0400

    Fixed question issue
    Added a <p> tag around the question text
    Floated the question score and <p>
    Added a clear both on the div for the button.

--- questions/question_show.php
+++ questions/question_show.php
@@ -55,7 +55,7 @@ if ($result = $mysqli->query($query)) {
     /* fetch object array */
     while ($obj = $result->fetch_object()) {
 		printf("<div class=\"question\">");
-		printf("<div class=\"question-text\"><span class=\"question-score\">%s</span>%s</div>", $obj->score, urldecode($obj->question));
+		printf("<div class=\"question-text\"><span class=\"question-score\">%s</span><p>%s</p></div>", $obj->score, urldecode($obj->question));
         if(!$voting_closed and !$viewonly) { printf("<div class=\"voting-buttons\"><a href=\"#\" id=\"%s\" class=\"vote vote-up\" name=\"up\">Up vote</a>&nbsp;&nbsp;<a href=\"#\" id=\"%s\" class=\"vote vote-down\" name=\"down\">Down vote</a></div>", $obj->id, $obj->id); }
         printf("</div><hr>");
     }
