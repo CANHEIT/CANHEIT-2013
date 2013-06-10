52716fcabc0a1262e41434426bf76b6fa5b456ec
Jean-Ray Arseneau <jeanray@arseneau.ca>
2013-06-10 13:39:29 -0400

    Re-enabled refresh timeout

--- questions/index.php
+++ questions/index.php
@@ -211,7 +211,7 @@
 			};
 
 		$(document).ready(function() {
-		  //setTimeout("ReloadPage()", <?php echo $refresh; ?> );
+		  setTimeout("ReloadPage()", <?php echo $refresh; ?> );
 		  });
         </script>
     </body>
